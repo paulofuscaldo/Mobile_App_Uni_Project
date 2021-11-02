2c73f9c9838a71930ed0ad9d70296642a6b7ccc1
rober-web <45374701+rober-web@users.noreply.github.com>
2021-11-02 18:53:54 +0000

    Set style and components into the main screen (login)

--- app/src/main/java/com/example/project_test/MainActivity.java
+++ app/src/main/java/com/example/project_test/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         forgotPassword = (TextView) findViewById(R.id.forgotPassword);
         forgotPassword.setOnClickListener(this);
