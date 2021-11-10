bf29388454dcb708b6b661a5b847895a43ec7c71
rober-web <45374701+rober-web@users.noreply.github.com>
2021-11-10 22:02:04 +0000

    Added The Admin Adding Restaurant Screen

---
+++ app/src/main/java/com/example/project_test/FormAddRestaurantAdmin.java
@@ -0,0 +1,14 @@
+package com.example.project_test;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormAddRestaurantAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_add_restaurant_admin);
+    }
+}
